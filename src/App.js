bb5137e11a095d474a3970436550f7d4ceaa5520
Jordan Rhode <rhode.jordan@gmail.com>
2022-02-17 20:21:05 -0600

    getting tesseract to process image inputs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { createWorker } from 'tesseract.js';
+
+import { useState } from 'react';
+import Tesseract from 'tesseract.js';
 import './App.css';
 
 function App() {
-  const worker = createWorker({
-    logger: m => console.log(m),
-  });
-  const doOCR = async () => {
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    setOcr(text);
-  };
-  const [ocr, setOcr] = useState('Recognizing...');
-//   useEffect(() => {
-//     doOCR();
-//   });
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    Tesseract.recognize(imagePath, 'eng', { 
+        logger: m => console.log(m) 
+      }
+    )
+    .catch (err => {
+      console.error(err);
+    })
+    .then(result => {
+     console.log(result);
+     if (String(result.data.text).toLowerCase().includes('carbonated')) {
+       setText('this is carbonated');
+     }
+    else {
+      setText('this is not carbonated');
+    }})
+  }
+
   return (
     <div className="App">
-      <p>{ocr}</p>
-      <button onClick={() => doOCR()}>Process Image</button>
+      <img src={imagePath} className="App-image" alt="logo" style={{width:500}}/>
+      <div>{text}</div>
+      <input type="file" onInput={e => setImagePath(URL.createObjectURL(e.target.files[0]))} accept="image/*"/>
+      <button onClick={handleClick} style={{height:50}}> convert to text</button>
     </div>
   );
 }
